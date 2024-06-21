218993f9c9ba851e8d35a6b73189b9e219eb48bb
Shirahn <shirahn@gendac.co.za>
2024-06-21 07:32:30 +0200

    Added wait to be clickable function which fixed clicking non-rendered elements issue

--- GolfWebScraperApp.py
+++ GolfWebScraperApp.py
@@ -13,20 +13,19 @@ import time
 options = Options()
 options.add_experimental_option("detach", True)
 
-# Set up Edge browser to be automatically accessed
 driver = webdriver.Edge(service=Service(EdgeChromiumDriverManager().install()), options=options)
 
 driver.get('https://lastminutegolf.co.za/')
 
 WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'searchCourseMob')))
 
-
 searchInput = driver.find_element(By.ID, "searchCourseMob" )
 searchInput.send_keys("Cape Town")
 searchInput.send_keys(Keys.RETURN)
 
-courses = driver.find_elements(By.CLASS_NAME, 'btnBookNow ')
-
+WebDriverWait(driver, 10).until(
+        EC.presence_of_all_elements_located((By.CLASS_NAME, 'dateRangePickerFilter'))
+)
 datePickers = driver.find_elements(By.CLASS_NAME, 'dateRangePickerFilter')
 sundayDatePicker = [datePicker for datePicker in datePickers if 'Sun' in datePicker.text]
 sundayDatePicker = sundayDatePicker[0]
@@ -35,28 +34,17 @@ driver.execute_script("arguments[0].classList.add('active');", sundayDatePicker)
 WebDriverWait(driver, 10).until(EC.element_to_be_clickable(sundayDatePicker)) 
 sundayDatePicker.click()
 
-if len(courses) > 0:
-    # Select the first course in the list of courses. 
-    courses[0].click()
+WebDriverWait(driver, 10).until(
+        EC.presence_of_all_elements_located((By.CLASS_NAME, 'btnBookNow'))
+)
+WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, 'btnBookNow')))
+courses = driver.find_elements(By.CLASS_NAME, 'btnBookNow')
 
-    #Wait until the li element containing 'Sun' has been loaded, then find these elements. 
-    WebDriverWait(driver, 10).until(
-        EC.presence_of_all_elements_located((By.CLASS_NAME, 'dateRangePickerFilter'))
-    )
-    datePickers = driver.find_elements(By.CLASS_NAME, 'dateRangePickerFilter')
-    sundayDatePicker = [datePicker for datePicker in datePickers if 'Sun' in datePicker.text]
-    sundayDatePicker = sundayDatePicker[0]
+coursetest = courses[1]
 
-    driver.execute_script("arguments[0].classList.add('active');", sundayDatePicker)
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable(sundayDatePicker)) 
-    sundayDatePicker.click()
-    #Get the appropriate information from the page 
+coursetest.click()
 
 
-    #Repeat for the rest of the courses in the list. 
-
-
-
-
-else:
-        print("No buttons found to click")
+#for course in courses:
+ #   WebDriverWait(driver, 10).until(EC.element_to_be_clickable(course))
+  #  course.click()
