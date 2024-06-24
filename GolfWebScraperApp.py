06ad31ec24202f1b4a62a7084bee74af5fa0cbc5
Shirahn <shirahn@gendac.co.za>
2024-06-24 15:21:36 +0200

    Uses calender to choose date picker as opposed to buttons. Buttons were used since browser was opened in a small window and was being interpreted as a mobile browser.

--- GolfWebScraperApp.py
+++ GolfWebScraperApp.py
@@ -9,29 +9,33 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 import time
+import datetime
 
 options = Options()
 options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+driver.maximize_window()
 driver.get('https://lastminutegolf.co.za/')
 
 WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'searchCourseMob')))
 
-searchInput = driver.find_element(By.ID, "searchCourseMob")
+searchInput = driver.find_element(By.ID, "searchCourse")
 searchInput.send_keys("Cape Town")
 searchInput.send_keys(Keys.RETURN)
 
-WebDriverWait(driver, 10).until(
-        EC.presence_of_all_elements_located((By.CLASS_NAME, 'dateRangePickerFilter'))
-)
-datePickers = driver.find_elements(By.CLASS_NAME, 'dateRangePickerFilter')
-sundayDatePicker = [datePicker for datePicker in datePickers if 'Sun' in datePicker.text]
-sundayDatePicker = sundayDatePicker[0]
+today = datetime.date.today()
+daysUntilSunday = (6 - today.weekday()) % 7
+if daysUntilSunday == 0: daysUntilSunday = 7
+nextSunday = today + datetime.timedelta(days=daysUntilSunday) 
 
-driver.execute_script("arguments[0].classList.add('active');", sundayDatePicker)
-WebDriverWait(driver, 10).until(EC.element_to_be_clickable(sundayDatePicker))
-sundayDatePicker.click()
+WebDriverWait(driver, 10).until(
+        EC.presence_of_all_elements_located((By.CLASS_NAME, 'ui-datepicker-calendar'))
+)
+
+xpath = f"//td[a/text()='{nextSunday.day}']"
+datePickerElement = driver.find_element(By.XPATH, xpath)
+datePickerElement.click()
 
 WebDriverWait(driver, 10).until(
         EC.presence_of_all_elements_located((By.CLASS_NAME, 'btnBookNow'))
